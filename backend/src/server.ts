775a9d38131174076af898ccd54964c3415591b3
01supertips <01supertips@gmail.com>
2022-10-26 09:41:06 -0300

    remove modules

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,34 @@
-import { serverHttp} from "./http"
-import "./websocket"
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+require('dotenv')
+import * as dotenv from 'dotenv'
+import { router } from './routes'
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
 
 
-serverHttp.listen(3001,() =>{
+app.use(cors())
+
+app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            eror: err.message
+        })
+    }
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error.'
+
+    })
+})
+
+
+app.listen(3001, () => {
     console.log("Rodando na porta 3001")
 })
