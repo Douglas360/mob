f6167f4b10f66d074fa5975b824a164e16f8f22d
01supertips <01supertips@gmail.com>
2022-11-25 22:17:13 -0300

    update

--- backend/src/services/Jogo/ListResultService.ts
+++ backend/src/services/Jogo/ListResultService.ts
@@ -16,16 +16,16 @@ var today = moment().format('YYYY-MM-DD h:m:s')
 class ListResultService {
 
     async execute(minuto: String, id_liga: number) {
-        const result = await prismaClient.$queryRaw`
+    /*    const result = await prismaClient.$queryRaw`
         select top 20 * from t_jogo
         where (id_liga=${id_liga}
         and minuto_jogo like ${`${minuto}%`}
         and dt_atualizacao >= '2022-11-25')
 
         order by id_jogo desc 
-        `
+        `*/
 
-     /*     const result = await prismaClient.$queryRaw`
+          const result = await prismaClient.$queryRaw`
               select top 20 * from t_jogo 
                   where (id_liga=${id_liga}
                   and minuto_jogo like ${`${minuto}%`} 
@@ -33,7 +33,7 @@ class ListResultService {
                   
                   order by id_jogo desc
             
-              `*/
+              `
 
 
         //  await setRedis(`minuto-${minuto}`, JSON.stringify(result)) // Seta os dados no cache
