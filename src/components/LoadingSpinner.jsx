4979091f762251cc498607a1fd5b5c3b9f254e2c
FlavioSant <flaviohenrique_santos@hotmail.com>
2022-11-28 23:03:07 -0300

    :wrench: Corrigido ordenacao dos dados se hora atual for maior que 22h

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ export const LoadingSpinner = () => {
     <div role="status">
       <svg
         aria-hidden="true"
-        class="mr-2 w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-sky-600"
+        className="mr-2 w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-sky-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
