a3c29ae97b641a4d3426ec51ba28688bfb32c611
01supertips <01supertips@gmail.com>
2022-12-19 21:07:05 -0300

    Adicionado LandingPage

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,12 +3,12 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 export const api = axios.create({
-  //baseURL: 'https://api.supertips.com.br',
-    baseURL: "http://192.168.1.96:3001",
+  baseURL: 'https://api.supertips.com.br',
+  //baseURL: "http://192.168.1.96:3001",
   headers: {
     Authorization: `Bearer ${token}`,
   },
 });
 
 //export const Urldomanin = "http://localhost:3001" 
-export const Urldomanin = "https://supertips.com.br"
+export const Urldomanin = "https://api.supertips.com.br"
